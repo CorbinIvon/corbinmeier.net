44c255a2b29d4dc3b9b764b39356051dade541de
Corbin Meier <corbinivon@gmail.com>
2025-10-12 22:01:26 -0700

    Updates portfolio description and add image reference

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.parchment.com", "www.butte.edu", "edent.github.io"],
+    domains: [
+      "www.parchment.com",
+      "www.butte.edu",
+      "edent.github.io",
+      // allow private user images from GitHubusercontent (used by some project images)
+      "private-user-images.githubusercontent.com",
+    ],
     // Or use remotePatterns if you prefer a pattern-based allow list:
     // remotePatterns: [
     //   {
